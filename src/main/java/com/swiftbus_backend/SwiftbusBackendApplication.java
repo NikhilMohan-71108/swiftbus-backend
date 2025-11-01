64e699432fb3c46a31345caf7d4f7a15916d4323
Nikhil Mohan <nikhilmohan.11@gmail.com>
2025-11-01 19:04:37 +0530

    Initial commit - SwiftBus backend (Clean Architecture)

---
+++ src/main/java/com/swiftbus_backend/SwiftbusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftbusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftbusBackendApplication.class, args);
+	}
+
+}
